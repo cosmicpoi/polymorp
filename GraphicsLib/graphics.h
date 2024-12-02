6d4264613c6fed4352d55ddcc1cd26e74e25e35b
Timothy <timmelenj@gmail.com>
2024-12-01 21:02:30 -0500

    added Glfw and glad test for a bsic triangle

--- GraphicsLib/graphics.h
+++ GraphicsLib/graphics.h
@@ -1,8 +1,10 @@
 class GraphicsContext {
 public:
+    /*
     void initialize();  // Set up OpenGL or Metal
     void clearScreen(); // Clear the frame buffer
     void drawRectangle(const Vector2& position, const Vector2& size, const Color& color);
     void drawSprite(const Sprite& sprite, const Transform& transform);
     void present();     // Swap buffers
+    */
 };
