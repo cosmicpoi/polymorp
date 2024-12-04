f4acbab6ef4023931753e565f5ef78cc6095ee38
Timothy <timmelenj@gmail.com>
2024-12-03 23:25:23 -0500

    Added GLGraphics.h that uses Triangle.h

--- main.cpp
+++ main.cpp
@@ -1,10 +1,46 @@
 #include "SnakeGame.h"
-#include "GLTest.h"
+#include "GLGraphics.h"
+#include "Triangle.h"
+#include "UnitLib/Vector.h"
 #include <iostream>
 
 int main()
 {
-    return RunGL();
+    GLGraphics gl;
+    const unsigned int SCR_WIDTH = 800;
+    const unsigned int SCR_HEIGHT = 600;
+    Vector3<float> backGroundColor = {0.3f, 0.3f, 0.4f};
+   
+    if (!gl.Initialize(SCR_WIDTH, SCR_HEIGHT, "Test Windwo")) {
+        return -1;
+    }
+    if(!gl.BuildShaders()){
+        return -1;
+    }
+    while (!gl.ShouldClose()) {
+
+        gl.ProcessScreen(backGroundColor[0], backGroundColor[1], backGroundColor[2]);
+        
+        //Draw a triangle
+        /*
+        float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // left  
+         0.5f, -0.5f, 0.0f, // right 
+         0.0f,  0.5f, 0.0f  // top   
+        }; */
+        
+        Vector3<float> p1{-0.5f, -0.5f, 0.0f};
+        Vector3<float> p2{ 0.5f, -0.5f, 0.0f};
+        Vector3<float> p3{0.0f,  0.5f, 0.0f};
+
+        Triangle triangle(p1, p2, p3);
+
+        gl.DrawTriangle(triangle);
+
+        gl.EndFrame();
+    }
+
+    return 0;
 
     // std::vector<std::string> games = {"snake", "gltest"};
 
